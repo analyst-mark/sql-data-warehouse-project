f2ed29a98ef216b3499cd76899571d5d4bd3e5f1
analyst-mark <127795776+analyst-mark@users.noreply.github.com>
2025-10-31 11:03:55 +0000

    Update loading the data.sql

--- scripts/loading the data.sql
+++ scripts/loading the data.sql
@@ -1,13 +1,17 @@
+-- Empty the table before loading updated data
+TRUNCATE TABLE bronze.crm_cust_info;
+
+-- Bulk load data from CSV into the Bronze table
 BULK INSERT bronze.crm_cust_info
 FROM 'C:\Users\mark\Documents\GitHub\sql-data-warehouse-project-test\datsets\source_crm\cust_info.csv'
 WITH (
-		firstrow = 2,
-		fieldterminator = ',',
-		tablock
-	
-
+    FIRSTROW = 2,              -- Skip header row
+    FIELDTERMINATOR = ',',     -- Columns separated by commas
+    TABLOCK                    -- Lock table for performance
 );
 
-SELECT * FROM bronze.crm_cust_info cci 
+-- Validate data load
+SELECT * FROM bronze.crm_cust_info cci;
 
-SELECT count(*) FROM bronze.crm_cust_info cci 
+-- Count number of records
+SELECT COUNT(*) FROM bronze.crm_cust_info cci;
